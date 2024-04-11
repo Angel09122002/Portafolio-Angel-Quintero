9df65fbe442c7f2b2d566eb90f0e8dc1643a6007
Angel09122002 <gabrielquintero1221@gmail.com>
2024-04-11 00:17:33 -0400

    resume update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ecommerce from "../assets/portfolio/ecommerProductPage.png";
+import flappyBird from "../assets/portfolio/flappyBird.png";
 import landingPageHappytails from "../assets/portfolio/landingPageHappytails.png";
 import littlelemon from "../assets/portfolio/littlelemon.png";
 import mouseover from "../assets/portfolio/mouseover.png";
@@ -9,11 +10,11 @@ const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      title: "LandingPage HappytailsPetsSpa",
-      src: landingPageHappytails,
+      title: "Pokedex-API",
+      src: pokedex,
       text: "",
-      githubLink: "https://github.com/Angel09122002/Happytailspetsspa",
-      demoLink: "https://willowy-phoenix-3480d3.netlify.app",
+      githubLink: "https://github.com/Angel09122002/Pokedexjs",
+      demoLink: "https://spectacular-liger-e9470a.netlify.app",
     },
     {
       id: 2,
@@ -31,17 +32,9 @@ const Portfolio = () => {
       githubLink: "https://github.com/Angel09122002/little-lemon-capstone",
       demoLink: "https://spiffy-gecko-d72cb0.netlify.app",
     },
-    {
-      id: 4,
-      title: "Pokedex-API",
-      src: pokedex,
-      text: "",
-      githubLink: "https://github.com/Angel09122002/Pokedexjs",
-      demoLink: "https://spectacular-liger-e9470a.netlify.app",
-    },
 
     {
-      id: 5,
+      id: 6,
       title: "Weather App-API",
       src: weatherapp,
       text: "",
@@ -50,7 +43,23 @@ const Portfolio = () => {
       demoLink: "https://weather-app-react-js-angel-quintero.netlify.app",
     },
     {
-      id: 6,
+      id: 7,
+      title: "LandingPage HappytailsPetsSpa",
+      src: landingPageHappytails,
+      text: "",
+      githubLink: "https://github.com/Angel09122002/Happytailspetsspa",
+      demoLink: "https://willowy-phoenix-3480d3.netlify.app",
+    },
+    {
+      id: 8,
+      title: "Flappy Bird Game Js  ",
+      src: flappyBird,
+      text: "",
+      githubLink: "https://github.com/Angel09122002/FlappyBird-game-JS",
+      demoLink: "https://flappy-birdgame-js.netlify.app",
+    },
+    {
+      id: 9,
       title: "MouseOver",
       src: mouseover,
       text: "",
@@ -70,7 +79,7 @@ const Portfolio = () => {
           <p className="py-6">Check out my work right here</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        <div className="grid sm:grid-cols-2 md:grid-cols-4 gap-5 px-12 sm:px-0">
           {portfolios.map(({ id, title, src, text, githubLink, demoLink }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
@@ -78,10 +87,10 @@ const Portfolio = () => {
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"
               />
-              <div className="p-4">
+              <div className="p-4 h-11">
                 <h1 className="font-bold text-black-500">{title}</h1>
                 <p className="text-lg font-bold pb-2"></p>
-                <p className="text-sm">{text}</p>
+                <p className="text-sm ">{text}</p>
               </div>
               <div className="flex items-center justify-center">
                 <a
